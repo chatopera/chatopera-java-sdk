3081c81984ebc982c9d844309443e6ec444dc38e
Hai Liang Wang <hain@chatopera.com>
2021-08-02 19:13:24 +0800

    Update docs generation

--- admin/docs.sh
+++ admin/docs.sh
@@ -13,11 +13,15 @@ rootDir=$(cd $baseDir/..;pwd)
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 cd $baseDir/..
 rm -rf target
-mvn clean compile javadoc:javadoc
+mvn clean site:site
+mvn compile javadoc:javadoc
 echo "API Docs is built successfully. path:" $rootDir/"target/site/apidocs"
 
 if [ -d ~/chatopera/chatopera-sample-java/docs ]; then
-  cd target/site/apidocs
+  echo "Move docs into ~/chatopera/chatopera-sample-java/docs ..."
+  rm -rf ~/chatopera/chatopera-sample-java/docs
+  mkdir ~/chatopera/chatopera-sample-java/docs
+  cd target/site
   tar cf - .|(cd ~/chatopera/chatopera-sample-java/docs;tar xf -)
 fi
 
