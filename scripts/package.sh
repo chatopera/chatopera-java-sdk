861b95e69f79652b779d92cd9a2cd25c42dfe173
Hai Liang Wang <hain@chatopera.com>
2022-07-08 02:38:59 +0100

    Enable build a fat jar as sdk

---
+++ scripts/package.sh
@@ -0,0 +1,18 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+cwdDir=$PWD
+export PYTHONUNBUFFERED=1
+export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+export TS=$(date +%Y%m%d%H%M%S)
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+mvn -DskipTests clean package
